82e768d3317ab486a94308135517b39fc7b0f17c
AhmedArbaz <ah2797764@gmail.com>
2024-11-27 16:24:03 +0500

    little changes of hero section for responsivness and also add package react-type-animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,25 +4,29 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 lg:font-extrabold text-6xl">
-            Hero, I am Ahmed
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center sm:text-left">
+          <h1 className="text-white mb-4 sm:text-5xl lg:font-extrabold text-6xl">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-pink-600 ">Hello I am {" "} 
+            </span>
+             Ahmed
           </h1>
-          <p className="text-[#ADB7BE] mb-6 text-lg lg:text-xl">
+          <p className="text-[#ADB7BE] text-base mb-6 sm:text-lg lg:text-xl">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius quae
             culpa expedita hic nulla alias quam veritatis odit doloremque
             distinctio, itaque dicta beatae.
           </p>
           <div>
-            <button className="px-6 rounded-full mr-4 py-3 bg-white hover:bg-slate-200 text-black">Hire me</button>
-            <button className="px-6 rounded-full py-3 bg-transparent hover:bg-slate-900 text-white border border-white mt-3">Download CV</button>
+            <button className="px-10 rounded-full w-full sm:w-fit mr-4 py-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">Hire me</button>
+            <button className="px-1 rounded-full w-full sm:w-fit py-1 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-900 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-900 rounded-full px-6 py-3">Download CV</span>
+              </button>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-          <div className="rounded-full bg-[#242424] w-[400px] h-[400px] relative">
+          <div className="rounded-full bg-[#242424] w-[350px] h-[350px] relative">
             <Image
-              className="mix-blend-screen absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
+              className="mix-blend-color-dodge absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
               src="/images/avatar.jpg"
               alt="avatar"
               width={300}
